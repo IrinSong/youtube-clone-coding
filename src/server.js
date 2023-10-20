2a42afc7f9a5a0eeb9a1d3019f033694bb0032df
Irin Song <minnpicc@gmail.com>
2023-10-20 16:59:41 +0900

    Add external middleware

--- src/server.js
+++ src/server.js
@@ -1,13 +1,10 @@
 // const express = require("express");
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
-
-const looger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   return res.send("Home");
@@ -16,7 +13,7 @@ const handleLogin = (req, res) => {
   return res.send("Login here");
 };
 
-app.use(looger);
+app.use(logger); //순서가 중요! middleware를 전체에 적용하려면 맨 위에 있어야 함.
 app.get("/", handleHome);
 app.get("/login", handleLogin);
 
