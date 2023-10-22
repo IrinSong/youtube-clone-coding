3a22cf0dcc67dfd69447b9fd212ad64e304bb62c
Irin Song <minnpicc@gmail.com>
2023-10-22 10:29:48 +0900

    Add controllers

---
+++ src/controllers/videoController.js
@@ -0,0 +1,3 @@
+export const home = (req, res) => res.send("Recommended Videos");
+export const watchVideo = (req, res) => res.send("Watch Video");
+export const editVideo = (req, res) => res.send("Edit Video");
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { home } from "../controllers/videoController";
+import { join } from "../controllers/userController";
 
 const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
+
+globalRouter.get("/", home);
+globalRouter.get("join", join);
 
 export default globalRouter;
