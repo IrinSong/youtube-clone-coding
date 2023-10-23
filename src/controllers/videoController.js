d199215b1a69be7d235785a1f4688f76b2dd0174
Irin Song <minnpicc@gmail.com>
2023-10-23 21:17:00 +0900

    Extend templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,5 +2,5 @@ export const home = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Searching Videos");
 export const upload = (req, res) => res.send("Upload New Videos");
 export const watch = (req, res) => res.render("Watch");
-export const edit = (req, res) => res.send("Edit Video");
+export const edit = (req, res) => res.render("edit");
 export const remove = (req, res) => res.send("Delete Video");
