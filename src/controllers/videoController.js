6892b515f28afd93f35252cd1941ea01fb7380e3
Irin Song <minnpicc@gmail.com>
2023-10-23 20:55:07 +0900

    Add partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
 export const home = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Searching Videos");
 export const upload = (req, res) => res.send("Upload New Videos");
-export const watch = (req, res) => res.send(`Watch Video #${req.params.id}`);
+export const watch = (req, res) => res.render("Watch");
 export const edit = (req, res) => res.send("Edit Video");
 export const remove = (req, res) => res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger); //순서가 중요! middleware를 전체에 적용하려면 맨 위에 있어야 함.
 app.use("/", globalRouter);
 app.use("/users", userRouter);
