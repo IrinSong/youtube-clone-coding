f9ae9c3904b1f1ebeae6c67bbaba1046f9b4661c
Irin Song <minnpicc@gmail.com>
2023-10-28 10:09:59 +0900

    Variables to templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,4 @@
-export const home = (req, res) => res.render("home");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Searching Videos");
 export const upload = (req, res) => res.send("Upload New Videos");
 export const watch = (req, res) => res.render("Watch");
