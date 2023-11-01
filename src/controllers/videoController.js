6c89d7143cf21a6096ac57da904c0583e8cb4cd5
Irin Song <minnpicc@gmail.com>
2023-11-01 22:37:11 +0900

    Fisrt query

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,66 +1,31 @@
-let videos = [
-  {
-    title: "Fisrt Video",
-    rating: 5,
-    comments: 2,
-    createdAt: 2,
-    views: 59,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: 2,
-    views: 1,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: 2,
-    views: 59,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const home = (req, res) =>
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  const videos = await Video.find({}); // await -> database에게 결과값을 받을 때까지 기다려줌.
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const search = (req, res) => res.send("Searching Videos");
 
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = (req, res) => {
-  const newVideo = {
-    title: req.body.title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
   // const id = req.parmas.id;
-  const video = videos[id - 1];
-  return res.render("Watch", { pageTitle: `Watching: ${video.title}`, video });
+  return res.render("Watch", { pageTitle: `Watching` });
 };
 
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body; // express.urlencoded({ extended: true })가 있기에 가능.
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
---
+++ src/init.js
@@ -0,0 +1,10 @@
+import app from "./server";
+import "./db";
+import videoModel from "./models/Video";
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
