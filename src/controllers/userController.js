c64984ae007d5ae3c1dccdccedfa5866e3e146b0
Irin Song <minnpicc@gmail.com>
2023-11-18 22:29:18 +0900

    Form validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,22 @@ import User from "../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const usernameExists = await User.exists({ username: username });
+  const emailExists = await User.exists({ email: email });
+  if (usernameExists) {
+    return res.render("join", { pageTitle: "Join", errorMessage: "This username is already taken." });
+  }
+  if (emailExists) {
+    return res.render("join", { pageTitle: "Join", errorMessage: "This e-mail is already taken." });
+  }
+  // const exists = await User.exists({ $or: [{ username: username }, { email: email }] });
+  // if (exists) {
+  //   return res.render("join", { pageTitle: "Join", errorMessage: "This e-mail OR username is already taken." });
+  // }
+  if (password !== password2) {
+    return res.render("join", { pageTitle: "Join", errorMessage: "Password confirmation does not match." });
+  }
   await User.create({
     name,
     username,
