9d9a2999f4e2b27f32d4e68d421f8c1b4bf4736d
Irin Song <minnpicc@gmail.com>
2023-11-21 16:08:17 +0900

    Logged in user part

---
+++ src/middlewares.js
@@ -0,0 +1,7 @@
+export const localsMiddleware = (req, res, next) => {
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
+  res.locals.siteName = "Metube";
+  res.locals.loggedInUser = req.session.user;
+  console.log(res.locals);
+  next();
+};
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 // const express = require("express");
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -13,6 +15,16 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger); //순서가 중요! middleware를 전체에 적용하려면 맨 위에 있어야 함.
 app.use(express.urlencoded({ extended: true })); // express application이 form의 value들을 javascript object 형식으로 이해할 수 있도록 함.
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
