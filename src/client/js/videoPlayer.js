a9af4793f0847bcadd7cc120a1b844ea8f6665b3
Irin Song <minnpicc@gmail.com>
2023-12-30 18:05:59 +0900

    Duration and current time

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,7 +1,8 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 const volumeRange = document.getElementById("volume");
 
 let volumeValue = 0.5;
@@ -38,6 +39,15 @@ const handleVolumeChange = (e) => {
   video.volume = value;
 };
 
+const handleVideoMeta = () => {
+  totalTime.innerText = Math.floor(video.duration);
+};
+const handleVideoTime = () => {
+  totalTime.innerText = Math.floor(video.currentTime);
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleVideoMeta);
+video.addEventListener("timeupdate", handleVideoTime);
