dfbb678045fa8275009e2ff0180525db2f67e963
Irin Song <minnpicc@gmail.com>
2023-12-30 17:45:02 +0900

    Volume

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,7 +4,10 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
-const handlePlayBtn = (e) => {
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlayClick = (e) => {
   if (video.paused) {
     video.play();
   } else {
@@ -13,15 +16,28 @@ const handlePlayBtn = (e) => {
   playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handleMuteBtn = (e) => {
+const handleMuteClick = (e) => {
   if (video.muted) {
     video.muted = false;
   } else {
     video.muted = true;
   }
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-  volumeRange.value = video.muted ? 0 : 0.5;
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
-playBtn.addEventListener("click", handlePlayBtn);
-muteBtn.addEventListener("click", handleMuteBtn);
+const handleVolumeChange = (e) => {
+  const {
+    target: { value },
+  } = e;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
